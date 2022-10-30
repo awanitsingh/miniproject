7f524cf7e1460f58718c244e8e096df892a86aff
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-10-30 13:28:53 -0700

    omegle

--- miniProject1.py
+++ miniProject1.py
@@ -3,7 +3,7 @@ import random
 user_action = input("Enter a choice (rock, paper, scissors): ")
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
-print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+print(f"\n You choose {user_action}, computer choose {computer_action}.\n")
 
 if user_action == computer_action:
     print(f"Both players selected {user_action}. It's a tie!")
---
+++ whilePractice.py
@@ -0,0 +1,8 @@
+i = 1
+
+while i < 100:
+    print(i)
+    if i == 5:
+        break
+    i += 4
+
