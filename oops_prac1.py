c95c92ef5a4bde8837ffed7fc7b51ff2fc714ffc
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-12-11 15:50:54 +0530

    oops practice

---
+++ oops_prac1.py
@@ -0,0 +1,85 @@
+# class laptop:
+#     def __init__(self,name,processor):
+#         self.name = name
+#         self.processor = processor
+#     def printoutput(self):
+#         print("My laptop name is : ",self.name,"and the processor is : ",self.processor)
+# laptop1 = laptop("Asus","i7")
+# laptop2 = laptop("HP","i5")
+# laptop1.printoutput()
+# laptop2.printoutput()
+
+# print(id(laptop1))
+# print(id(laptop2))
+
+
+# class Info:
+#     def __init__(self,name,reg):
+#         self.name = name
+#         self.reg = reg
+#     def printoutput(self):
+#         print("My name is : ",self.name,"and my reg.no is : ",self.reg)
+
+# person1 = Info("Awanit Kumar Singh","12218575")
+# person1.printoutput()       
+
+
+# class person:
+#     def __init__(self):
+#         self.name = "Awanit"
+#         self.age = 18
+
+# person1 = person()
+# person2 = person()
+
+# person2.name = "Babusaheb"
+
+# print(person1.name)
+# print(person2.name)
+
+
+# class person:
+#     def __init__(self):
+#         self.name = "Awanit"
+#         self.age = 18
+
+#     def updatename(self,name):
+#         self.name = name    
+
+# person1 = person()
+# person2 = person()
+
+# person1.updatename("Babusaheb")
+
+# print(person1.name)
+# print(person2.name)
+
+
+# class person:
+#     def __init__(self):
+#         self.name = "Awanit"
+#         self.age = 18
+
+#     def updatename(self,name):
+#         self.name = name    
+
+#     def compareAge(self,other):
+#         if self.age == other.age:
+#             return True
+#         else:
+#             return False        
+
+# person1 = person()
+# person2 = person()
+
+# person1.age = 22
+
+# if person1.compareAge(person2):
+#     print("They are of same")
+# else:
+#     print("They are not same")    
+
+# print(person1.name,person1.age)
+# print(person2.name,person2.age)
+
+
