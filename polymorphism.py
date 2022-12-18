51088e3a5ecf24e1eafbde5706717d6699d2f438
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-12-18 16:31:22 +0530

    Polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,77 @@
+a = "Hello"
+b = [1,2,3,4,5,6]
+
+print(len(b))
+
+def add(a,b,c = 0):
+    return a+b+c
+
+print(add(1,2))    
+print(add(1,2,3))
+
+class Rect:
+    def calculateArea(self,length=None,breadth=None):
+        if length != None and breadth != None:
+            return length*breadth
+        elif length != None:
+            return length*length
+
+rectangle = Rect()        
+print(rectangle.calculateArea(2,3))
+print(rectangle.calculateArea(2))
+
+
+class Bird:
+    def category(self):
+        print("This is a category of bird")
+    
+    def fly(self):
+        print("I can fly")
+
+
+class Sparrow(Bird):
+    def sizeParameter(self):
+        print("i am small in size")
+class Crow(Bird):
+    pass
+class Ostrich(Bird):
+    def fly(self):
+        print("I cannot fly, sorry")
+
+objBird = Bird()
+objSparrow = Sparrow()
+objCrow = Crow()
+objOstrich = Ostrich()
+
+objBird.category()
+objBird.fly()
+objCrow.category()
+objCrow.fly()
+objSparrow.category()
+objSparrow.fly()
+objOstrich.category()
+objOstrich.fly()
+
+
+
+class vehicle:
+    def capacity(self):
+        return "seating capacity of a car is 4 passengers" 
+class car1(vehicle):
+    pass
+class car2(vehicle):
+    pass
+
+class bus:
+    def capacity(self):
+        return "seating capacity of a bus is 50 passengers"
+
+obj = vehicle()     
+
+obj1 = car1()
+obj2 = car2()
+obj3 = bus()
+
+print(obj1.capacity())
+print(obj2.capacity())
+print(obj3.capacity())
