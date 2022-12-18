63813965e799f496674e45954f8d21f3a29af108
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-12-18 13:50:37 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+class A:
+    def __init__(self):
+        print("This is init of method A")
+
+    def method1(self):
+        print("This is method 1")
+
+class B:
+    def method2(self):
+        print("This is method 2")
+
+# class C(B,A):
+#     def method3(self):
+#         print("This is method 3")
+
+
+objA = A()        
+# objB = B()
+# objC = C()
+
+# objC.method1()
+
