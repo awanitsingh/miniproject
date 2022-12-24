52a0b4d357e3739d8310d2e5417b5a14a44dbc24
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-12-24 12:58:59 +0530

    Files Problems

---
+++ files_1.py
@@ -0,0 +1,44 @@
+#f = open("demo.txt" , "r")
+#print(f.read())
+# print(f.readline())
+
+# f1 = open("demo1.txt","w")
+# f1.write("This is second file")
+
+# f2 = open("demo2.txt","w")
+# f2.write("My name is Awanit Kumar Singh \nI am 18 years old \nMy college name is Lovely Professional University")
+
+# for i in f:
+#     f1.write(i)
+
+# try:
+#     f = open("demo.txt")
+    # your code goes here
+#finally:
+#    f.close()
+# This way we are making sure that file is closed properly even if exception is raised that cause the progrm flow to sto
+
+#with open("demo.txt") as f:
+#    f.read() #-->  example
+    # Your code goes here
+
+# f = open("demo.txt","r")
+# print(f.read(10))  
+# print(f.tell())  
+
+
+# f = open("edu.avif","rb")
+# f1 = open("edu_copy.avif","wb")
+
+# print(f.read())
+
+# for i in f:
+#     f1.write(i)
+
+
+# import os
+
+# if os.path.exists("demo4.txt"):
+#     os.remove("demo4.txt")
+# else:
+#     print("File does not exixt")    
