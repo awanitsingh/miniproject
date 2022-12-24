e12b025779fe481524ab577d22a175b6691c16b9
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2022-12-24 13:46:53 +0530

    Exception Handling

---
+++ exceptionHandling.py
@@ -0,0 +1,30 @@
+#try:
+    # This block of code will run and throw errors if there are any
+
+#except:
+    # This will raise the errors
+   
+#else:
+    # This will execute if there are no errors
+
+#finally:
+    # This will execute regardless the result of try and execept
+
+
+
+# try:
+#     f = open("demo.py")    
+#     try:
+#         f.write("ABC")
+#     except:
+#         print("Error in file")    
+#     finally:
+#         f.close()
+# except:
+#     print("Can't open file")        
+
+
+
+# a = 5
+# if a < 10:
+#     raise Exception("Value is less than 5")
